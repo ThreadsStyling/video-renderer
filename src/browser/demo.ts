fb4555331f58aebf47d52bbc18097cbf4af8a19e
Forbes Lindesay <forbes.lindesay@threadsstyling.com>
2018-07-26 11:49:07 +0100

    Handle time and handle changing assets over time

--- src/browser/demo.ts
+++ src/browser/demo.ts
@@ -2,6 +2,7 @@
 // and https://stackoverflow.com/questions/4073303/can-i-have-a-video-with-transparent-background-using-html5-video-tag
 
 import './register/overlay';
+import './register/trim';
 import {AssetKind, loadAsset, filterComplex, render} from './';
 
 const canvas = document.createElement('canvas');
@@ -10,19 +11,41 @@ document.body.appendChild(canvas);
 Promise.all([
   loadAsset(require('../node/__tests__/Video Of People Walking.mp4'), AssetKind.Video),
   loadAsset(require('../node/__tests__/loop.mp4'), AssetKind.VideoWithAlpha),
-]).then((inputs) =>
-  render(
+]).then((inputs) => {
+  const f = (x: number, y: number) =>
     filterComplex(inputs, [
       {
         inputs: ['0', '1'],
-        outputs: [],
+        outputs: ['overlaid'],
         name: 'overlay',
+        args: {x, y},
+      },
+      {
+        inputs: ['overlaid'],
+        outputs: [],
+        name: 'trim',
         args: {
-          x: 500,
-          y: 100,
+          start: 0,
+          end: 5,
         },
       },
-    ]),
-    canvas,
-  ),
-);
+    ]);
+  const player = render(canvas, f(500, 100));
+  canvas.addEventListener(
+    'mousemove',
+    (e) => {
+      e.preventDefault();
+      player.setAsset(f(e.clientX - inputs[1].width / 2, e.clientY - inputs[1].height / 2));
+    },
+    false,
+  );
+  canvas.addEventListener(
+    'touchmove',
+    (e) => {
+      e.preventDefault();
+      const touch = e.touches[0];
+      player.setAsset(f(touch.clientX - inputs[1].width / 2, touch.clientY - inputs[1].height / 2));
+    },
+    false,
+  );
+});
--- src/browser/render.ts
+++ src/browser/render.ts
@@ -1,9 +1,15 @@
 import Asset from './Asset';
 
-export default function render(asset: Asset, canvas: HTMLCanvasElement) {
-  canvas.width = asset.width;
-  canvas.height = asset.height;
+interface RenderState {
+  currentTime: number;
+}
+export default function render(canvas: HTMLCanvasElement, asset?: Asset) {
+  if (asset) {
+    canvas.width = asset.width;
+    canvas.height = asset.height;
+  }
   const ctx = canvas.getContext('2d')!;
+  const handlers: Array<(state: RenderState) => void> = [];
 
   let start = Date.now();
   let disposed = false;
@@ -11,8 +17,7 @@ export default function render(asset: Asset, canvas: HTMLCanvasElement) {
   let playing = true;
   requestAnimationFrame(draw);
   function draw() {
-    if (paused || disposed) {
-      playing = false;
+    if (!asset) {
       return;
     }
     const now = Date.now();
@@ -24,9 +29,29 @@ export default function render(asset: Asset, canvas: HTMLCanvasElement) {
     const frame = asset.getFrame(time);
     ctx.clearRect(0, 0, asset.width, asset.height);
     ctx.drawImage(frame.canvas, 0, 0);
-    requestAnimationFrame(draw);
+    if (handlers.length) {
+      const state = {currentTime: time};
+      handlers.forEach((h) => h(state));
+    }
+    if (paused || disposed) {
+      playing = false;
+    } else {
+      requestAnimationFrame(draw);
+    }
   }
   return {
+    setAsset(_asset: Asset) {
+      asset = _asset;
+      canvas.width = asset.width;
+      canvas.height = asset.height;
+      if (!playing) {
+        playing = true;
+        requestAnimationFrame(draw);
+      }
+    },
+    getAsset() {
+      return asset;
+    },
     isPaused() {
       return paused;
     },
@@ -46,8 +71,24 @@ export default function render(asset: Asset, canvas: HTMLCanvasElement) {
         requestAnimationFrame(draw);
       }
     },
+    seek(time: number) {
+      start = Date.now() - time;
+      if (!playing) {
+        playing = true;
+        requestAnimationFrame(draw);
+      }
+    },
     dispose() {
       disposed = true;
+    },
+    onFrame(handler: (state: RenderState) => void) {
+      handlers.push(handler);
+      let unsubscribed = false;
+      return () => {
+        if (unsubscribed) return;
+        unsubscribed = true;
+        handlers.splice(handlers.indexOf(handler), 1);
+      };
     },
   };
 }
