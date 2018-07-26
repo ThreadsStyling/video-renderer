028df5744eb280ef72d12ec6b1e163fc859c8d36
Forbes Lindesay <forbes.lindesay@threadsstyling.com>
2018-07-26 15:53:29 +0100

    Fix iOS rendering

--- src/browser/demo.ts
+++ src/browser/demo.ts
@@ -6,6 +6,12 @@ import './register/trim';
 import {AssetKind, loadAsset, filterComplex, render} from './';
 
 const canvas = document.createElement('canvas');
+
+canvas.style.margin = 'auto';
+// canvas.style.width = '100vmin';
+canvas.style.display = 'block';
+const style = document.createElement('style');
+document.head.appendChild(style);
 document.body.appendChild(canvas);
 
 Promise.all([
@@ -31,11 +37,40 @@ Promise.all([
       },
     ]);
   const player = render(canvas, f(500, 100));
+
+  const ratio = (100 * inputs[0].height) / inputs[0].width;
+  style.textContent = `
+    html, body {
+      margin: 0;
+      padding: 0;
+    }
+    body {
+      height: 100vh;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: black;
+    }
+    canvas {
+      height: 100vh;
+    }
+    @media (min-height: ${ratio}vw) {
+      canvas {
+        width: 100vw;
+        height: auto;
+      }
+    }
+  `;
+  function update(e: {clientX: number; clientY: number}) {
+    const x = (e.clientX - canvas.offsetLeft) * (canvas.width / canvas.offsetWidth);
+    const y = (e.clientY - canvas.offsetTop) * (canvas.height / canvas.offsetHeight);
+    player.setAsset(f(x - inputs[1].width / 2, y - inputs[1].height / 2));
+  }
   canvas.addEventListener(
     'mousemove',
     (e) => {
       e.preventDefault();
-      player.setAsset(f(e.clientX - inputs[1].width / 2, e.clientY - inputs[1].height / 2));
+      update(e);
     },
     false,
   );
@@ -44,7 +79,7 @@ Promise.all([
     (e) => {
       e.preventDefault();
       const touch = e.touches[0];
-      player.setAsset(f(touch.clientX - inputs[1].width / 2, touch.clientY - inputs[1].height / 2));
+      update(touch);
     },
     false,
   );
