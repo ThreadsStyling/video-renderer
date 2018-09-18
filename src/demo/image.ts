256d3ecef93feb0bf16eaa14fd5d92fd08685437
streamich <streamich@gmail.com>
2018-09-18 13:15:14 +0200

    style: 💄 run prettier

--- src/demo/image.ts
+++ src/demo/image.ts
@@ -10,8 +10,12 @@ canvas.style.display = 'block';
 document.body.appendChild(canvas);
 
 Promise.all([
-  Asset.fromImage('https://user-images.githubusercontent.com/9773803/45625491-45dbfc00-ba8d-11e8-8c9c-e961e08eaa99.png'),
-  Asset.fromImage('https://user-images.githubusercontent.com/3481367/45488425-4ad14080-b759-11e8-9629-2fb02283f02e.png'),
+  Asset.fromImage(
+    'https://user-images.githubusercontent.com/9773803/45625491-45dbfc00-ba8d-11e8-8c9c-e961e08eaa99.png',
+  ),
+  Asset.fromImage(
+    'https://user-images.githubusercontent.com/3481367/45488425-4ad14080-b759-11e8-9629-2fb02283f02e.png',
+  ),
   Asset.fromImage('https://d1algegyh2h2u2.cloudfront.net/svg/landing/threads-logo-white.svg'),
 ]).then((inputs) => {
   const f = (x: number, y: number) =>
