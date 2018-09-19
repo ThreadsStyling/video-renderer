abc03b0b62e8e470a56fe2b6d126a073197b663f
streamich <streamich@gmail.com>
2018-09-19 18:37:27 +0200

    feat: 🎸 implement scale filter

--- src/browser/filters/index.ts
+++ src/browser/filters/index.ts
@@ -2,6 +2,7 @@ import Asset from '../Asset';
 import trim from './trim';
 import overlay from './overlay';
 import drawtext from './drawtext';
+import scale from './scale';
 
 export type Filter = (frames: Asset[], args: {[key: string]: string | number}) => Asset[];
 export interface FiltersByName {
@@ -11,4 +12,5 @@ export const filters: FiltersByName = {
   trim,
   overlay,
   drawtext,
+  scale,
 };
---
+++ src/browser/filters/scale.ts
@@ -0,0 +1,28 @@
+import {Filter} from '.';
+import Asset from '../Asset';
+import createCanvasAndContext from '../util/createCanvasAndContext';
+
+const scale: Filter = ([asset], {w, h}) => {
+  const [canvas, context] = createCanvasAndContext();
+
+  canvas.width = w as any | 0;
+  canvas.height = h as any | 0;
+
+  const render = (time: number) => {
+    asset.renderFrame(time);
+    context.drawImage(asset.canvas, 0, 0, asset.width, asset.height, 0, 0, canvas.width, canvas.height);
+  };
+
+  return [
+    new Asset(
+      asset.duration,
+      canvas.width,
+      canvas.height,
+      canvas,
+      context,
+      render,
+    ),
+  ];
+};
+
+export default scale;
