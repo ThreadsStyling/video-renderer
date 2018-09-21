9360075bd03fb4f1052607e1b68813c226dcdfc5
streamich <streamich@gmail.com>
2018-09-21 22:50:39 +0200

    test: 💍 implement test runner and check for diff threshold

--- test/image.tests.js
+++ test/image.tests.js
@@ -46,45 +46,67 @@ const renderFfmpeg = async ({name, assets, filters}) => {
   return getFileAsDataUrl(outFile);
 };
 
+const runTestcase = async (testcase) => {
+  const {name, assets, filters} = testcase;
+
+  const ffmpegResultDataUrl = await renderFfmpeg(testcase);
+  const assetFiles = [];
+
+  for (const asset of assets) {
+    assetFiles.push(getFileAsDataUrl(asset));
+  }
+
+  const result = await inChrome({
+    // debug: true,
+    func: async (module, args) => {
+      return await module.runTest(...args);
+    },
+    args: [assetFiles, filters, ffmpegResultDataUrl],
+    module: path.join(__dirname, 'image.module.js'),
+  });
+
+  const dataUrl = result.canvasResult.dataUrl;
+  const outFile = path.join(outDir, `${name}-canvas.png`);
+  const data = new Buffer(dataUrl.substr(dataUrl.indexOf(',') + 1), 'base64');
+  fs.writeFileSync(outFile, data);
+
+  if (!result.diffResult) {
+    throw new Error(`Image dimensions don't match <canvas> = (${reult.canvasResult.width}, ${reult.canvasResult.height}); ffmpeg = (${reult.ffmpegResult.width}, ${reult.ffmpegResult.height})`);
+  }
+
+  const dataUrlDiff = result.diffResult.dataUrl;
+  const dataUrlAll = result.diffResult.dataUrlCombined;
+  const outFileDiff = path.join(outDir, `${name}-diff.png`);
+  const outFileAll = path.join(outDir, `${name}-all.png`);
+  const dataDiff = new Buffer(dataUrlDiff.substr(dataUrlDiff.indexOf(',') + 1), 'base64');
+  const dataAll = new Buffer(dataUrlAll.substr(dataUrlAll.indexOf(',') + 1), 'base64');
+  fs.writeFileSync(outFileDiff, dataDiff);
+  fs.writeFileSync(outFileAll, dataAll);
+
+  const threshold = 3;
+  if (result.diffResult.diff > threshold) {
+    throw new Error(`Discrepancy ${result.diffResult.percent} is more than ${threshold}% threshold.`);
+  }
+
+  return result.diffResult.percent;
+};
+
 const main = async () => {
+  const passed = true;
+
   for (const testcase of testcases) {
-    const {name, assets, filters} = testcase;
+    try {
+      const percent = await runTestcase(testcase);
+      console.log(`✅  Passed [${testcase.name}], difference ${percent}`);
+    } catch (error) {
+      passed = false;
+      console.log(`❌  Failed [${testcase.name}]: ${error.message}`);
+      console.error(error);
+    };
+  }
 
-    const ffmpegResultDataUrl = await renderFfmpeg(testcase);
-    const assetFiles = [];
-
-    for (const asset of assets) {
-      assetFiles.push(getFileAsDataUrl(asset));
-    }
-
-    const result = await inChrome({
-      debug: true,
-      func: async (module, args) => {
-        return await module.runTest(...args);
-      },
-      args: [assetFiles, filters, ffmpegResultDataUrl],
-      module: path.join(__dirname, 'image.module.js'),
-    });
-
-    console.log('diff', result.diffResult.percent);
-
-    const dataUrl = result.canvasResult.dataUrl;
-    const outFile = path.join(outDir, `${name}-canvas.png`);
-    const data = new Buffer(dataUrl.substr(dataUrl.indexOf(',') + 1), 'base64');
-    fs.writeFileSync(outFile, data);
-
-    if (!result.diffResult) {
-      throw new Error(`Image dimensions don't match <canvas> = (${reult.canvasResult.width}, ${reult.canvasResult.height}); ffmpeg = (${reult.ffmpegResult.width}, ${reult.ffmpegResult.height})`);
-    }
-
-    const dataUrlDiff = result.diffResult.dataUrl;
-    const dataUrlAll = result.diffResult.dataUrlCombined;
-    const outFileDiff = path.join(outDir, `${name}-diff.png`);
-    const outFileAll = path.join(outDir, `${name}-all.png`);
-    const dataDiff = new Buffer(dataUrlDiff.substr(dataUrlDiff.indexOf(',') + 1), 'base64');
-    const dataAll = new Buffer(dataUrlAll.substr(dataUrlAll.indexOf(',') + 1), 'base64');
-    fs.writeFileSync(outFileDiff, dataDiff);
-    fs.writeFileSync(outFileAll, dataAll);
+  if (!passed) {
+    process.exit(1);
   }
 };
 
