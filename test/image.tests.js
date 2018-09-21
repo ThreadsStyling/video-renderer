5c560fbbf612f1aecda26fa707dbf623325b63e9
streamich <streamich@gmail.com>
2018-09-21 22:42:16 +0200

    test: 💍 compute image diff

--- test/image.tests.js
+++ test/image.tests.js
@@ -12,7 +12,7 @@ try {
 
 const dataUrlCache = new Map();
 
-const getAssetDataUrl = (filename) => {
+const getFileAsDataUrl = (filename) => {
   if (!dataUrlCache.has(filename)) {
     dataUrlCache.set(filename, 'data:image/png;base64,' + fs.readFileSync(filename).toString('base64'));
   }
@@ -37,43 +37,54 @@ const ffmpeg = async (...args) => {
 
 const renderFfmpeg = async ({name, assets, filters}) => {
   const inputs = ['-y'];
-
   for (const asset of assets) {
     inputs.push('-i');
     inputs.push(asset);
   }
-
   const outFile = path.join(outDir, `${name}-ffmpeg.png`);
   await ffmpeg(...inputs, '-filter_complex', filterComplex(filters), outFile);
+  return getFileAsDataUrl(outFile);
 };
 
 const main = async () => {
   for (const testcase of testcases) {
     const {name, assets, filters} = testcase;
 
-    await renderFfmpeg(testcase);
-
+    const ffmpegResultDataUrl = await renderFfmpeg(testcase);
     const assetFiles = [];
 
     for (const asset of assets) {
-      assetFiles.push(getAssetDataUrl(asset));
+      assetFiles.push(getFileAsDataUrl(asset));
     }
 
     const result = await inChrome({
-      // debug: true,
-      func: async (module, [assetFiles, filters]) => {
-        const png = await module.renderImage(assetFiles, filters);
-        return png;
+      debug: true,
+      func: async (module, args) => {
+        return await module.runTest(...args);
       },
-      args: [assetFiles, filters],
+      args: [assetFiles, filters, ffmpegResultDataUrl],
       module: path.join(__dirname, 'image.module.js'),
     });
 
+    console.log('diff', result.diffResult.percent);
+
+    const dataUrl = result.canvasResult.dataUrl;
     const outFile = path.join(outDir, `${name}-canvas.png`);
-    const data = new Buffer(result.substr(result.indexOf(',') + 1), 'base64');
+    const data = new Buffer(dataUrl.substr(dataUrl.indexOf(',') + 1), 'base64');
     fs.writeFileSync(outFile, data);
 
-    // console.log();
+    if (!result.diffResult) {
+      throw new Error(`Image dimensions don't match <canvas> = (${reult.canvasResult.width}, ${reult.canvasResult.height}); ffmpeg = (${reult.ffmpegResult.width}, ${reult.ffmpegResult.height})`);
+    }
+
+    const dataUrlDiff = result.diffResult.dataUrl;
+    const dataUrlAll = result.diffResult.dataUrlCombined;
+    const outFileDiff = path.join(outDir, `${name}-diff.png`);
+    const outFileAll = path.join(outDir, `${name}-all.png`);
+    const dataDiff = new Buffer(dataUrlDiff.substr(dataUrlDiff.indexOf(',') + 1), 'base64');
+    const dataAll = new Buffer(dataUrlAll.substr(dataUrlAll.indexOf(',') + 1), 'base64');
+    fs.writeFileSync(outFileDiff, dataDiff);
+    fs.writeFileSync(outFileAll, dataAll);
   }
 };
 
