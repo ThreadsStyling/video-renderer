1984ff854df4fbfb546190ee534f6ee46d9fbaab
streamich <streamich@gmail.com>
2018-09-21 12:46:14 +0200

    perf: ⚡️ improve rotate filter

--- src/browser/filters/rotate.ts
+++ src/browser/filters/rotate.ts
@@ -2,48 +2,33 @@ import {Filter} from '.';
 import Asset from '../Asset';
 import createCanvasAndContext from '../util/createCanvasAndContext';
 
-/**
- * @param sCanvas Source <canvas>.
- * @param sContext Context of source <canvas>.
- * @param rotation Rotation amount in Radians.
- */
-const getRotatedCanvasAndContext = (sCanvas: HTMLCanvasElement, width: number, height: number, angle: number, background: string): [HTMLCanvasElement, CanvasRenderingContext2D] => {
-  const [canvas, context] = createCanvasAndContext();
-
-  canvas.width = width;
-  canvas.height = height
-
-  context.fillStyle = background;
-  context.fillRect(0, 0, width, height);
-  context.translate(width / 2, height / 2);
-  context.rotate(1);
-  context.drawImage(sCanvas, -sCanvas.width / 2, -sCanvas.height / 2);
-
-  return [canvas, context];
-}
-
 const rotate: Filter = ([asset], {angle, out_w, out_h, fillcolor = '#000000'}) => {
   const [canvas, context] = createCanvasAndContext();
-  const outWidth = out_w ? out_w as any | 0 : asset.width;
-  const outHeight = out_h ? out_h as any | 0 : asset.height;
+  const width = out_w ? out_w as any | 0 : asset.width;
+  const height = out_h ? out_h as any | 0 : asset.height;
+  const angleInRadians = angle as any | 0;
 
-  canvas.width = outWidth;
-  canvas.height = outHeight;
+  canvas.width = width;
+  canvas.height = height;
 
   const render = (time: number) => {
     asset.renderFrame(time);
 
-    const [rotatedCanvas] = getRotatedCanvasAndContext(asset.canvas, outWidth, outHeight, angle as any | 0, fillcolor as string);
-
-    context.clearRect(0, 0, outWidth, outHeight);
-    context.drawImage(rotatedCanvas, 0, 0);
+    context.clearRect(0, 0, width, height);
+    context.save();
+    context.fillStyle = String(fillcolor);
+    context.fillRect(0, 0, width, height);
+    context.translate(width / 2, height / 2);
+    context.rotate(angleInRadians);
+    context.drawImage(asset.canvas, -asset.canvas.width / 2, -asset.canvas.height / 2);
+    context.restore();
   };
 
   return [
     new Asset(
       asset.duration,
-      canvas.width,
-      canvas.height,
+      width,
+      height,
       canvas,
       context,
       render,
