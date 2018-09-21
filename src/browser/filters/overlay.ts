6640b505e488307a25775b53b565ab4e6714450c
streamich <streamich@gmail.com>
2018-09-21 12:27:18 +0200

    fix: 🐛 don't mutate background input in overlay filter
    "overlay" filter was mutating in-place its background input.

--- src/browser/filters/overlay.ts
+++ src/browser/filters/overlay.ts
@@ -1,27 +1,33 @@
 import {Filter} from '.';
 import Asset from '../Asset';
+import createCanvasAndContext from '../util/createCanvasAndContext';
 
-const overlay: Filter = (inputs, args) => {
-  const x = +(args.x || 0);
-  const y = +(args.y || 0);
-  const backgroundAsset = inputs[0];
-  const background = backgroundAsset.context;
-  const foreground = inputs[1].canvas;
-  const renderBackground = backgroundAsset.renderFrame;
-  const renderForeground = inputs[1].renderFrame;
+const overlay: Filter = ([background, foreground], args) => {
+  const x = args.x as any | 0;
+  const y = args.y as any | 0;
+  const {width, height} = background;
+  const [canvas, context] = createCanvasAndContext();
+
+  canvas.width = width;
+  canvas.height = height;
+
+  const render = (time: number) => {
+    background.renderFrame(time);
+    foreground.renderFrame(time);
+
+    context.clearRect(0, 0, width, height);
+    context.drawImage(background.canvas, 0, 0);
+    context.drawImage(foreground.canvas, x, y);
+  };
 
   return [
     new Asset(
-      backgroundAsset.duration,
-      backgroundAsset.width,
-      backgroundAsset.height,
-      backgroundAsset.canvas,
-      background,
-      (time) => {
-        renderBackground(time);
-        renderForeground(time);
-        background.drawImage(foreground, x, y);
-      },
+      background.duration,
+      width,
+      height,
+      canvas,
+      context,
+      render,
     ),
   ];
 };
