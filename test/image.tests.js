3a8fe012633670e600a85e18eccbb09bf88af08c
streamich <streamich@gmail.com>
2018-09-21 21:56:58 +0200

    test: 💍 render test cases with ffmpeg

--- test/image.tests.js
+++ test/image.tests.js
@@ -1,7 +1,14 @@
 const testcases = require('./image.testcases');
 const {execute: inChrome} = require('puppet-master');
+const {filterComplex} = require('../lib/node');
+const {spawn} = require('child_process');
 const fs = require('fs');
 const path = require('path');
+
+const outDir = path.join(__dirname, 'image-tests-out');
+try {
+  fs.mkdirSync(outDir);
+} catch (err) {}
 
 const dataUrlCache = new Map();
 
@@ -13,9 +20,39 @@ const getAssetDataUrl = (filename) => {
   return dataUrlCache.get(filename);
 };
 
+const ffmpegBinary = path.join(__dirname, '..', 'src', 'node', '__tests__', 'bin', 'ffmpeg');
+const ffmpeg = async (...args) => {
+  const ffmpegProc = spawn(ffmpegBinary, args, {
+    stdio: 'ignore'
+  });
+
+  return new Promise((resolve, reject) => {
+    ffmpegProc.on('error', reject);
+    ffmpegProc.on('close', (code) => {
+      if (code !== 0) reject(new Error('FFMPEG extited with code ' + code));
+      else resolve();
+    });
+  });
+}
+
+const renderFfmpeg = async ({name, assets, filters}) => {
+  const inputs = ['-y'];
+
+  for (const asset of assets) {
+    inputs.push('-i');
+    inputs.push(asset);
+  }
+
+  const outFile = path.join(outDir, `${name}-ffmpeg.png`);
+  await ffmpeg(...inputs, '-filter_complex', filterComplex(filters), outFile);
+};
+
 const main = async () => {
   for (const testcase of testcases) {
-    const {assets, filters} = testcase;
+    const {name, assets, filters} = testcase;
+
+    await renderFfmpeg(testcase);
+
     const assetFiles = [];
 
     for (const asset of assets) {
@@ -32,7 +69,7 @@ const main = async () => {
       module: path.join(__dirname, 'image.module.js'),
     });
 
-    const outFile = path.join(__dirname, 'test.png');
+    const outFile = path.join(outDir, `${name}-canvas.png`);
     const data = new Buffer(result.substr(result.indexOf(',') + 1), 'base64');
     fs.writeFileSync(outFile, data);
 
