a7086daf869e6ee5e87961c914f8fa1b5b47770e
streamich <streamich@gmail.com>
2018-09-24 09:19:18 +0200

    feat: 🎸 add "blend" complex filter
    This is first implementation, more work has to be done on mapping ffmpeg
    blending modes to Canvas blending modes. Also Canvas draws transparent
    border arround the image when blending mode is used.

---
+++ src/demo/crop2.ts
@@ -0,0 +1,26 @@
+import {filterComplex, render, Asset} from '../browser';
+
+const canvas = document.createElement('canvas');
+
+canvas.style.width = '100px';
+canvas.style.height = '100px';
+
+canvas.style.margin = 'auto';
+canvas.style.display = 'block';
+document.body.appendChild(canvas);
+
+Promise.all([
+  Asset.fromImage(require('../../assets/servers.png')),
+]).then((inputs) => {
+  render(canvas, filterComplex(inputs, [
+    {
+      name: 'crop',
+      args: {
+        w: 200,
+        h: 200,
+        x: 0,
+        y: 0,
+      },
+    },
+  ]));
+});
