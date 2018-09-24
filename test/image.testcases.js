e3776f00152cd782bdac0fddd6c354bc5a7f7ef2
streamich <streamich@gmail.com>
2018-09-24 08:27:16 +0200

    test: 💍 add "pad" filter functional test

--- test/image.testcases.js
+++ test/image.testcases.js
@@ -251,6 +251,32 @@ module.exports = [
       }
     ],
   },
+  {
+    name: 'pad-transparent-bg',
+    assets: [
+      join(assetDir, 'image.jpg'),
+      join(assetDir, 'servers.png'),
+    ],
+    filters: [
+      {
+        inputs: ['1'],
+        name: 'pad',
+        args: {
+          w: 600,
+          h: 400,
+          x: 20,
+          y: 20,
+          color: '#00000000',
+        },
+        outputs: ['padded'],
+      },
+      {
+        inputs: ['0', 'padded'],
+        name: 'overlay',
+        args: {x: 0, y: 0},
+      }
+    ],
+  },
   /* ffmpeg crashes on this, possibly needs some flag
   {
     name: 'drawtext',
