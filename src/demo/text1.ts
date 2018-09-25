98b52b1ae85528afdf1366b914a48dd163603911
streamich <streamich@gmail.com>
2018-09-25 22:11:27 +0200

    test: 💍 add ability to run demos in Chrome, add text demo

---
+++ src/demo/text1.ts
@@ -0,0 +1,24 @@
+// Run: ts-node src/demo/text1.ts
+
+import renderDemo from './util/renderDemo';
+
+renderDemo(
+  [
+    'image.jpg',
+  ], [
+    {
+      inputs: ['0'],
+      name: 'drawtext',
+      args: {
+        text: 'SOME TEXT',
+        x: 100,
+        y: 100,
+        fontsize: 72,
+        fontcolor: 'white',
+        box: 1,
+        boxcolor: 'black',
+        boxborderw: 10
+      }
+    }
+  ]
+).catch(console.error); // tslint:disable-line
