dc6a8153a8ab47c89a8980c8865bbe344ee11905
streamich <streamich@gmail.com>
2018-09-25 23:33:31 +0200

    fix: 🐛 don't mutate input of "drawtext" filter

--- src/browser/filters/drawtext.ts
+++ src/browser/filters/drawtext.ts
@@ -1,5 +1,6 @@
 import {Filter} from '.';
 import Asset from '../Asset';
+import createCanvasAndContext from '../util/createCanvasAndContext';
 
 const MAGIC_Y_END = 4.2;
 
@@ -8,43 +9,57 @@ function parse(path: string) {
 }
 
 const drawtext: Filter = ([background], args) => {
-  const x = +(args.x || 0);
-  const y = +(args.y || 0);
-  const text = String(args.text) || '';
+  const [canvas, context] = createCanvasAndContext();
+  const {width, height} = background;
+
+  canvas.width = width;
+  canvas.height = height;
+  context.textBaseline = 'top';
 
   // We use font file name as font face.
   const font = parse(String(args.fontfile)) || '';
 
+  const x = +(args.x || 0);
+  const y = +(args.y || 0);
+  const text = String(args.text) || '';
   const fontsize = +(args.fontsize || 0);
   const fontcolor = String(args.fontcolor) || '';
   const box = String(args.box) === '1';
   const boxborderw = +(args.boxborderw || 0);
   const boxcolor = String(args.boxcolor) || '';
 
+  const render = (time: number) => {
+    background.renderFrame(time);
+
+    // Render background.
+    context.clearRect(0, 0, width, height);
+    context.drawImage(background.canvas, 0, 0);
+
+    // Render text.
+    context.font = `${fontsize}px ${font}`;
+    if (box) {
+      const measurement = context.measureText(text);
+
+      context.fillStyle = boxcolor;
+      context.fillRect(
+        x - boxborderw,
+        y - boxborderw,
+        measurement.width + boxborderw * 2,
+        fontsize - fontsize / MAGIC_Y_END + boxborderw * 2,
+      );
+    }
+    context.fillStyle = fontcolor;
+    context.fillText(text, x, y - 2);
+  };
+
   return [
     new Asset(
       background.duration,
-      background.width,
-      background.height,
-      background.canvas,
-      background.context,
-      (time) => {
-        background.renderFrame(time);
-        background.context.font = `${fontsize}px ${font}`;
-        if (box) {
-          background.context.fillStyle = boxcolor;
-          const details = background.context.measureText(text);
-          background.context.fillRect(
-            x - boxborderw,
-            y - boxborderw,
-            details.width + boxborderw * 2,
-            fontsize - fontsize / MAGIC_Y_END + boxborderw * 2,
-          );
-        }
-        background.context.fillStyle = fontcolor;
-        background.context.textBaseline = 'top';
-        background.context.fillText(text, x, y - 2);
-      },
+      width,
+      height,
+      canvas,
+      context,
+      render,
     ),
   ];
 };
