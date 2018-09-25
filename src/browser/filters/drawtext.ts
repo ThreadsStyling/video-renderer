d5c5360013ad7a8314d5d8b6de2f8dd3eb45543d
streamich <streamich@gmail.com>
2018-09-25 23:05:39 +0200

    fix: 🐛 position "drawtext" background same as ffmpeg

--- src/browser/filters/drawtext.ts
+++ src/browser/filters/drawtext.ts
@@ -1,8 +1,7 @@
 import {Filter} from '.';
 import Asset from '../Asset';
 
-const MAGIC_Y_START = 3.2;
-const MAGIC_Y_END = 3.3;
+const MAGIC_Y_END = 4.2;
 
 function parse(path: string) {
   return (path.split('/').pop() || '').split('.').shift();
@@ -16,9 +15,9 @@ const drawtext: Filter = ([background], args) => {
   // We use font file name as font face.
   const font = parse(String(args.fontfile)) || '';
 
-  const fontsize = String(args.fontsize) || '';
+  const fontsize = +(args.fontsize || 0);
   const fontcolor = String(args.fontcolor) || '';
-  const box = +(args.box || 0);
+  const box = String(args.box) === '1';
   const boxborderw = +(args.boxborderw || 0);
   const boxcolor = String(args.boxcolor) || '';
 
@@ -33,14 +32,13 @@ const drawtext: Filter = ([background], args) => {
         background.renderFrame(time);
         background.context.font = `${fontsize}px ${font}`;
         if (box) {
-          const numericFontSize = Number(fontsize);
           background.context.fillStyle = boxcolor;
           const details = background.context.measureText(text);
           background.context.fillRect(
             x - boxborderw,
-            y - numericFontSize + numericFontSize / MAGIC_Y_START - boxborderw,
+            y - boxborderw,
             details.width + boxborderw * 2,
-            numericFontSize - numericFontSize / MAGIC_Y_END + boxborderw * 2,
+            fontsize - fontsize / MAGIC_Y_END + boxborderw * 2,
           );
         }
         background.context.fillStyle = fontcolor;
