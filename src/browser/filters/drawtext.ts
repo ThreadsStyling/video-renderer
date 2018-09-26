4428ad87f3ac39406a744dfcf25d2352d28c4c1a
streamich <streamich@gmail.com>
2018-09-26 09:41:21 +0200

    feat: 🎸 evaluate "drawtext" expressions in "x" and "y" args
    This commit also adds perf optimization where text is rendered only once
    and copied to the correct place on every re-render.

--- src/browser/filters/drawtext.ts
+++ src/browser/filters/drawtext.ts
@@ -1,8 +1,10 @@
 import {Filter} from '.';
 import Asset from '../Asset';
 import createCanvasAndContext from '../util/createCanvasAndContext';
+import {Parser} from 'expr-eval';
 
 const MAGIC_Y_END = 4.2;
+const parser = new Parser();
 
 function parse(path: string) {
   return (path.split('/').pop() || '').split('.').shift();
@@ -10,17 +12,16 @@ function parse(path: string) {
 
 const drawtext: Filter = ([background], args) => {
   const [canvas, context] = createCanvasAndContext();
+  const [textCanvas, textContext] = createCanvasAndContext();
   const {width, height} = background;
 
   canvas.width = width;
   canvas.height = height;
-  context.textBaseline = 'top';
 
   // We use font file name as font face.
   const font = parse(String(args.fontfile)) || '';
-
-  const x = +(args.x || 0);
-  const y = +(args.y || 0);
+  const xExpression = parser.parse(String(args.x));
+  const yExpression = parser.parse(String(args.y));
   const text = String(args.text) || '';
   const fontsize = +(args.fontsize || 0);
   const fontcolor = String(args.fontcolor) || '';
@@ -28,26 +29,33 @@ const drawtext: Filter = ([background], args) => {
   const boxborderw = +(args.boxborderw || 0);
   const boxcolor = String(args.boxcolor) || '';
 
+  // Measure text and evaluate expressions.
+  textContext.font = `${fontsize}px ${font}`;
+  const tw = textContext.measureText(text).width;
+  const expressionArgs = {
+    tw,
+    text_w: tw,
+  };
+  const x = xExpression.evaluate(expressionArgs);
+  const y = yExpression.evaluate(expressionArgs);
+
+  // Draw text on canvas once.
+  textCanvas.width = tw + (boxborderw * 2);
+  textCanvas.height = fontsize - (fontsize / MAGIC_Y_END) + (boxborderw * 2);
+  if (box) {
+    textContext.fillStyle = boxcolor;
+    textContext.fillRect(0, 0, textCanvas.width, textCanvas.height);
+  }
+  textContext.textBaseline = 'top';
+  textContext.font = `${fontsize}px ${font}`;
+  textContext.fillStyle = fontcolor;
+  textContext.fillText(text, boxborderw, boxborderw - 2);
+
   const render = (time: number) => {
-    // Render background.
     background.renderFrame(time);
     context.clearRect(0, 0, width, height);
     context.drawImage(background.canvas, 0, 0);
-
-    // Render text.
-    context.font = `${fontsize}px ${font}`;
-    if (box) {
-      const measurement = context.measureText(text);
-      context.fillStyle = boxcolor;
-      context.fillRect(
-        x - boxborderw,
-        y - boxborderw,
-        measurement.width + boxborderw * 2,
-        fontsize - fontsize / MAGIC_Y_END + boxborderw * 2,
-      );
-    }
-    context.fillStyle = fontcolor;
-    context.fillText(text, x, y - 2);
+    context.drawImage(textCanvas, x - boxborderw, y - boxborderw);
   };
 
   return [new Asset(background.duration, width, height, canvas, context, render)];
