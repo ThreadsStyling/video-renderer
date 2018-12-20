4d3559872186b0959a8aee53c2b9353c4a91fd62
Forbes Lindesay <forbes@lindesay.co.uk>
2018-12-20 14:19:54 +0000

    fix: clear canvas on each frame for scale (#28)
    Rendering scaled videos doesn't work unless you clear the canvas on each frame, because each frame just adds to the one before.

--- src/browser/filters/scale.ts
+++ src/browser/filters/scale.ts
@@ -5,13 +5,14 @@ import createCanvasAndContext from '../util/createCanvasAndContext';
 const scale: Filter = ([asset], {w, h}) => {
   const [canvas, context, dispose] = createCanvasAndContext();
 
-  canvas.width = w as any | 0;
-  canvas.height = h as any | 0;
+  const width = (canvas.width = w as any | 0);
+  const height = (canvas.height = h as any | 0);
 
   const render = (time: number, initialFrame: boolean) => {
     if (!(asset.renderFrame(time) || initialFrame)) {
       return false;
     }
+    context.clearRect(0, 0, width, height);
     context.drawImage(asset.canvas, 0, 0, asset.width, asset.height, 0, 0, canvas.width, canvas.height);
     return true;
   };
