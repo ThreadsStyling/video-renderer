51e46b85738658ac57074a29c4c6bd54266847d6
Diogo Cunha <diogofncunha@gmail.com>
2019-01-29 16:22:12 +0000

    fix: 🐛 clone loop assets to avoid dispose on base asset

--- src/browser/filters/loop.ts
+++ src/browser/filters/loop.ts
@@ -1,5 +1,8 @@
 import {Filter} from '.';
+import Asset from '../Asset';
 
-const loop: Filter = ([asset]) => [asset];
+const loop: Filter = ([asset]) => {
+  return [new Asset(asset.duration, asset.width, asset.height, asset.canvas, asset.context, asset.renderFrame)];
+};
 
 export default loop;
